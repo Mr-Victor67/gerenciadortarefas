58563a468afb3e98e25fe29459e0c93012f7f38b
Mr-Victor67 <victor.matos.santana@escola.pr.gov.br>
2026-07-28 12:34:56 -0300

    Add files via upload

---
+++ src/componentes/Footer/index.js
@@ -0,0 +1,20 @@
+import './index.css'
+
+function Footer(){
+    return (
+        <footer>
+            <div className="colegio">
+                <p clasName='nomeColegio'>
+               Colégio Santa Maria Goretti -2026
+               </p>
+            </div>
+            <div className="desenvolvedor">
+             <p ClassName='nomeDesenvolvedor'>
+                Desenvolvido por: Victor Hugo de Matos Santana
+             </p>
+            </div>
+
+        </footer>
+    )
+}
+export default Footer;
